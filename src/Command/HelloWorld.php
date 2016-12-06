007216b0c2c87f2bdba655618b852527b6c98706
Eduardo Nascimento <eduardo@eduardonascimento.com.br>
2016-12-06 19:47:33 -0200

    Add argument

--- src/Command/HelloWorld.php
+++ src/Command/HelloWorld.php
@@ -2,31 +2,39 @@
 namespace Talk\Command;
 
 use Symfony\Component\Console\Command\Command;
+use Symfony\Component\Console\Input\InputInterface;
+use Symfony\Component\Console\Input\InputArgument;
 
 class HelloWorld extends Command
 {
     protected $year;
+    protected $personName;
 
     protected function configure()
     {
         $this->setName('hello')
             ->setDescription('Says Hello!')
             ->setHelp("Trust me, I will not execute rm -rf /")
-            ->setHidden(false);
+            ->setHidden(false)
+            ->addArgument(
+                'name',
+                InputArgument::REQUIRED,
+                'The name of the person.'
+            );
     }
 
-    protected function initialize()
+    protected function initialize(InputInterface $input)
     {
         $this->year = 2016;
-    }
-
-    protected function interact()
-    {
-        echo "interact() called \n";
+        $this->personName = $input->getArgument('name');
     }
 
     protected function execute()
     {
-        echo sprintf("Hello PHP Conference %d! \n", $this->year);
+        echo sprintf(
+            "Hello %s, welcome to PHP Conference %d! \n",
+            $this->personName,
+            $this->year
+        );
     }
 }
