b337bf214f10e911809a422f4891c23b8b5d9e14
Eduardo Nascimento <eduardo@eduardonascimento.com.br>
2016-12-07 21:27:00 -0200

    Add Question Helper

--- src/Command/HelloWorld.php
+++ src/Command/HelloWorld.php
@@ -8,6 +8,7 @@ use Symfony\Component\Console\Input\InputArgument;
 use Symfony\Component\Console\Input\InputOption;
 use Symfony\Component\Console\Logger\ConsoleLogger;
 use Symfony\Component\Console\Helper\ProgressBar;
+use Symfony\Component\Console\Question\Question;
 
 class HelloWorld extends Command
 {
@@ -41,19 +42,32 @@ class HelloWorld extends Command
     {
         $this->logger = new ConsoleLogger($output);
         $this->progress = new ProgressBar($output, 100);
-        $this->progress->start();
 
         $this->year       = $input->getOption('year');
         $this->personName = $input->getArgument('name');
     }
 
-    protected function interact()
+    protected function interact(InputInterface $input, OutputInterface $output)
     {
-        $this->logger->debug(__METHOD__);
+        if (!$input->getArgument('name')) {
+            $question = new Question('Please supply a name:');
+            $question->setValidator(function ($name) {
+                if (empty($name)) {
+                    throw new \Exception('Name can not be empty');
+                }
+                return $name;
+            });
+            $answer = $this->getHelper('question')->ask($input, $output, $question);
+            $input->setArgument('name', $answer);
+            $this->personName = $input->getArgument('name');
+        }
     }
 
     protected function execute(InputInterface $input)
     {
+        $startTime = microtime(true);
+
+        $this->progress->start();
         for ($i=0; $i < 100; $i++) {
             usleep(rand(10000,100000));
             $this->progress->advance();
@@ -61,11 +75,14 @@ class HelloWorld extends Command
 
         $this->progress->finish();
 
+
         echo sprintf(
             "\nHello %s, welcome to PHP Conference %d! \n",
             $this->personName,
             $this->year
         );
 
+        $endTime = microtime(true);
+        $this->logger->info(sprintf("Run in: %f seconds", $endTime-$startTime));
     }
 }
